f8160f419af6f4b8e19734bf58564d1720ad6cff
Alexander Dudkin <raddanprofile@gmail.com>
2026-07-15 20:06:28 +0300

    #15: Add MapStruct mappers for Profile Service
    * feat: mapstruct mappers for profile-service
    
    * refactor: maven plugins refactor

---
+++ profile-service/src/main/java/com/ecommerce/profiles/mapper/CountryMapper.java
@@ -0,0 +1,16 @@
+package com.ecommerce.profiles.mapper;
+
+import com.ecommerce.profiles.entity.Country;
+import com.ecommerce.profiles.rest.dto.CountryResponse;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface CountryMapper {
+
+    List<CountryResponse> toListResponse(List<Country> countries);
+
+}
