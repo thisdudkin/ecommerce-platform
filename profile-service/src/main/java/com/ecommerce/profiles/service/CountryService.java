6434f18ff880f69041a708aa636032144c8519af
Alexander Dudkin <raddanprofile@gmail.com>
2026-07-16 15:40:21 +0300

    #19: Add an interface for the service layer in the Profile Service
    * feat: service layer in profile-service

---
+++ profile-service/src/main/java/com/ecommerce/profiles/service/CountryService.java
@@ -0,0 +1,9 @@
+package com.ecommerce.profiles.service;
+
+import com.ecommerce.profiles.rest.dto.CountryListResponse;
+
+public interface CountryService {
+
+    CountryListResponse listCountries();
+
+}
